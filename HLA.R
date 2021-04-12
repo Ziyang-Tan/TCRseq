0b4ff38b66e78c4e607afb7d8d4e90e0e598659c
tan <ziyang.tan@ki.se>
2021-04-12 10:03:36 +0200

    add HLA_frequency function

--- HLA.R
+++ HLA.R
@@ -5,8 +5,25 @@ tb <- bq_project_query('cradle-259115', sql)
 HLA <- bq_table_download(tb)
 
 library(dplyr)
+library(tibble)
 library(readr)
-tmp <- HLA %>% 
-  select(SampleID, A1, A2) %>% 
+HLA0201 <- HLA %>% 
   filter(grepl('^02:01', A1) | grepl('^02:01',A2))
-write_csv(tmp, 'HLA_0201.csv')
+#write_csv(tmp, 'HLA_0201.csv')
+
+HLA_frequency <- function(HLA_dat, allele){
+  # output a frequency table showing that in how many subjects a HLA type appears in 
+  # at least one of the alleles
+  # HLA_dat is HLA typing table from bigQuery
+  # allele is a vector of allele names
+  nameList <- HLA_dat %>% select(one_of(allele)) %>% unlist(use.names = F) %>% unique()
+  table <- lapply(nameList, function(x){
+    tmp <- HLA_dat %>% filter(get(allele[1]) == x | get(allele[2]) == x) %>% count()
+    return(tmp)
+  }) %>% bind_rows() %>% add_column(HLA = nameList) %>% arrange(desc(n))
+  return(table)
+}
+
+DRB <- HLA_frequency(HLA0201, c('DRB11', 'DRB12'))
+DPB <- HLA_frequency(HLA0201, c('DPB11', 'DPB12'))
+DQB <- HLA_frequency(HLA0201, c('DQB11', 'DQB12'))
